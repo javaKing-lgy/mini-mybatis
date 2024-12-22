792e720e76235464be84afaf433c7d59ce823591
liuguanyi <15143648189@163.com>
2024-12-22 16:52:57 +0800

    init(添加redis的key统一管理)

--- douyu-live-user-provider/src/main/java/com/douyu/live/user/provider/service/impl/IUserServiceImpl.java
+++ douyu-live-user-provider/src/main/java/com/douyu/live/user/provider/service/impl/IUserServiceImpl.java
@@ -1,10 +1,12 @@
 package com.douyu.live.user.provider.service.impl;
 
 import com.douyu.live.common.interfaces.utils.ConvertBeanUtils;
+import com.douyu.live.framework.redis.starter.key.UserProviderCacheKeyBuilder;
 import com.douyu.live.user.dto.UserDTO;
 import com.douyu.live.user.provider.dao.mapper.UserMapper;
 import com.douyu.live.user.provider.service.IUserService;
 import jakarta.annotation.Resource;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 /**
@@ -15,11 +17,20 @@ import org.springframework.stereotype.Service;
 public class IUserServiceImpl implements IUserService {
     @Resource
     private UserMapper userMapper;
+    @Resource
+    private RedisTemplate<String,UserDTO> redisTemplate;
+    @Resource
+    private UserProviderCacheKeyBuilder userProviderCacheKeyBuilder;
 
     @Override
     public UserDTO getByUserId(Long userId) {
         if(userId == null){
             return null;
+        }
+        String key = userProviderCacheKeyBuilder.buildUserInfoKey(userId);
+        UserDTO userDTO = redisTemplate.opsForValue().get(key);
+        if(userDTO != null){
+            return userDTO;
         }
         return ConvertBeanUtils.convert(userMapper.selectById(userId), UserDTO.class);
     }
