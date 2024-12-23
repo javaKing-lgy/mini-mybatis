a0e6433184e89178da57ba5add8d9758fca165b5
liuguanyi <15143648189@163.com>
2024-12-23 21:45:17 +0800

    将查询用户信息缓存到redis 完成批量查询

--- douyu-live-api/src/main/java/com/douyu/live/api/controller/UserController.java
+++ douyu-live-api/src/main/java/com/douyu/live/api/controller/UserController.java
@@ -6,6 +6,11 @@ import org.apache.dubbo.config.annotation.DubboReference;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 @RestController
 @RequestMapping(value = "/user")
 public class UserController {
@@ -16,6 +21,23 @@ public class UserController {
     @GetMapping("/getUserInfo")
     public UserDTO getUserInfo(Long userId) {
         return userRpc.getUserById(userId);
+    }
+    @GetMapping("/updateUserInfo")
+    public boolean updateUserInfo(UserDTO userDTO) {
+        return userRpc.updateUserInfo(userDTO);
+    }
+    @GetMapping("/insertOne")
+    public boolean insertOne(UserDTO userDTO) {
+        return userRpc.insertOne(userDTO);
+    }
+    @GetMapping("/batchQueryUserInfo")
+    public Map<Long,UserDTO> batchQueryUserInfo(String userIdStr) {
+        String[] idStr = userIdStr.split(",");
+        List<Long> userIdList = new ArrayList<>();
+        for (String userId : idStr) {
+            userIdList.add(Long.valueOf(userId));
+        }
+        return userRpc.batchQueryUserInfo(userIdList);
     }
 
 }
