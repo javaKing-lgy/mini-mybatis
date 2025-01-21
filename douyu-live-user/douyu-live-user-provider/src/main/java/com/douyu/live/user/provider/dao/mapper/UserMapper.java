5b1ec713dbc1a0a79fea10c89f99455a710a97d0
liuguanyi <15143648189@163.com>
2025-01-21 21:06:56 +0800

    --修改模块

--- douyu-live-user/douyu-live-user-provider/src/main/java/com/douyu/live/user/provider/dao/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.douyu.live.user.provider.dao.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.douyu.live.user.provider.dao.po.UserPO;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface UserMapper extends BaseMapper<UserPO> {
-}
