7e14c6b2c554c10d616fcf076aeb7f2ee74df8af
liuguanyi <15143648189@163.com>
2025-02-26 22:15:25 +0800

    feat(core): 实现分页查询功能
    - 新增 RowBounds 类用于表示分页参数
    - 在 Executor 接口中添加 rowBounds 参数
    - 修改 BaseExecutor 和 SimpleExecutor 类以支持分页查询
    - 新增 ResultContext 和 DefaultResultContext 类用于处理查询结果
    - 新增 ResultHandler 和 DefaultResultHandler接口和实现类
    - 修改 Configuration 类以支持分页查询- 新增 MapperBuilderAssistant 类用于构建 Mapper
    - 新增 ResultMap 和 ResultMapping 类用于结果映射
    - 新增 ResultSetWrapper 类用于包装结果集
    - 修改 BaseTypeHandler 和相关类型处理器以支持获取结果

--- src/main/java/cn/lgyjava/mybatis/session/defaults/DefaultSqlSession.java
+++ src/main/java/cn/lgyjava/mybatis/session/defaults/DefaultSqlSession.java
@@ -3,6 +3,7 @@ package cn.lgyjava.mybatis.session.defaults;
 import cn.lgyjava.mybatis.executor.Executor;
 import cn.lgyjava.mybatis.mapping.MappedStatement;
 import cn.lgyjava.mybatis.session.Configuration;
+import cn.lgyjava.mybatis.session.RowBounds;
 import cn.lgyjava.mybatis.session.SqlSession;
 import com.alibaba.fastjson.JSON;
 import org.slf4j.Logger;
@@ -38,7 +39,7 @@ public class DefaultSqlSession implements SqlSession {
 	public <T> T selectOne(String statement, Object parameter) {
 		logger.info("执行查询 statement：{} parameter：{}", statement, JSON.toJSONString(parameter));
 		MappedStatement ms = configuration.getMappedStatement(statement);
-		List<T> list = executor.query(ms, parameter, Executor.NO_RESULT_HANDLER, ms.getSqlSource().getBoundSql(parameter));
+		List<T> list = executor.query(ms, parameter, RowBounds.DEFAULT, Executor.NO_RESULT_HANDLER, ms.getSqlSource().getBoundSql(parameter));
 		return list.get(0);
 	}
 
